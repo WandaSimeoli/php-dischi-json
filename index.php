7696f25f5346ad527cab892abb39e42982c62b04
WandaSimeoli <wandasimeoli@gmail.com>
2023-08-24 17:26:51 +0200

    CDN Vue

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <title>PHP Dischi JSON</title>
 </head>
 <body>
-    
+    <div id="app">
+        {{ message }}
+    </div>
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="js/script.js"></script>
 </body>
 </html>
